ccd3c9430800b63516fecb53417883e6e2d067d5
carlosAC03 <cacostac03@educarex.es>
2025-04-08 18:48:06 +0200

    menu_externo

--- java1.js
+++ java1.js
@@ -1,15 +1,33 @@
-// Carrusel con transición suave entre imágenes cada 3 segundos
+// Carrusel de imágenes con transición
 const images = document.querySelectorAll('.carousel-img');
 let currentImageIndex = 0;
 
 function changeImage() {
-    // Quitar la clase "active" de todas las imágenes
     images.forEach(img => img.classList.remove('active'));
-
-    // Activar la siguiente imagen
     currentImageIndex = (currentImageIndex + 1) % images.length;
     images[currentImageIndex].classList.add('active');
 }
 
-// Cambia de imagen cada 3 segundos
 setInterval(changeImage, 3000);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const filtroBtn = document.querySelector('.btn-filtros');
+    const filtroMenu = document.querySelector('.filtro-menu');
+    const overlay = document.querySelector('.overlay');
+    const cerrarBtn = document.querySelector('.cerrar-menu');
+
+    filtroBtn.addEventListener('click', () => {
+        filtroMenu.classList.toggle('open');
+        overlay.classList.toggle('active');
+    });
+
+    cerrarBtn.addEventListener('click', () => {
+        filtroMenu.classList.remove('open');
+        overlay.classList.remove('active');
+    });
+
+    overlay.addEventListener('click', () => {
+        filtroMenu.classList.remove('open');
+        overlay.classList.remove('active');
+    });
+});

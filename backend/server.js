0b9c8518fc3637e7c5f19a6ad5322fd5db1bd2ca
carlosAC03 <cacostac03@educarex.es>
2025-05-06 20:28:29 +0200

    server

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+app.use(cors());
+
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+const dbName = "supermercado";
+
+app.get("/productos", async (req, res) => {
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const productos = await db.collection("productos").find().toArray();
+        res.json(productos);
+    } catch (err) {
+        res.status(500).send("Error al conectar con MongoDB");
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Servidor en http://localhost:3000");
+});

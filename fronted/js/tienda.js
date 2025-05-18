d0a47c5d9c859970eb96fb39a594d36920e4312c
carlosAC03 <125562558+carlosAC03@users.noreply.github.com>
2025-05-18 17:52:28 +0200

    url

--- fronted/js/tienda.js
+++ fronted/js/tienda.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     try {
         const API_URL = location.hostname === "localhost"
             ? "http://localhost:4000"
-            : "https://TU_BACKEND_RENDER.onrender.com"; // ← REEMPLAZA esto con tu URL real de Render
+            : "https://tfc-2gv2.onrender.com"; // ← REEMPLAZA esto con tu URL real de Render
 
         const res = await fetch(`${API_URL}/productos`);
         const data = await res.json();
